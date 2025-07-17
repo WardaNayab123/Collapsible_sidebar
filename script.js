f72675b4db50c482b2225a4b848138f5be6f6203
WardaNayab123 <wardanayab36@gmail.com>
2025-07-16 23:09:28 -0700

    collapsible sidebar

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.getElementById('toggleBtn').addEventListener('click', function () {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('collapsed');
+});
